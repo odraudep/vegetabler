ad8775c38228348d8a6933f183e7fba90edbae88
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2021-03-19 15:37:14 -0300

    section about done

---
+++ assets/js/dragslider.js
@@ -0,0 +1,38 @@
+const products = document.querySelector("section.products .products");
+
+dragSlider(products);
+
+function dragSlider(element) {
+  let
+    pressed = false,
+    startX,
+    scrollLeft;
+
+  element.addEventListener("mousedown", e => {
+    pressed = true;
+
+    startX = e.pageX - element.offsetLeft;
+    scrollLeft = element.scrollLeft;
+  });
+
+  element.addEventListener("mouseup", () => {
+    pressed = false;
+  });
+
+  element.addEventListener("mouseleave", () => {
+    pressed = false;
+  });
+
+  element.addEventListener("mousemove", e => {
+    if (!pressed)
+      return;
+
+    e.preventDefault();
+
+    const
+      x = e.pageX - element.offsetLeft,
+      step = x - startX;
+
+    element.scrollLeft = scrollLeft - step;
+  });
+}
